e26b947e08eb491f1585bcab4653c5f0383f69c6
lyajurvedi <>
2019-11-13 14:01:16 -0500

    update initial commit

--- dropbox-to-s3-node/config/myStoryConfigDB.js
+++ dropbox-to-s3-node/config/myStoryConfigDB.js
@@ -15,8 +15,8 @@ config.import.accessTokenDropBox = process.env.DROPBOX_ACCESS_TOKEN
 config.import.rootfoldernamepath_DropBox = process.env.DROPBOX_ROOT_FOLDER_FOR_DOWNLOAD
 
 //Dbx - BCH - direct
-//config.import.accessTokenDropBox = '64QeNKXSg7AAAAAAAAAAXV3TSluSoUPhyEAzn90fmdy1U31-o4jT7VOdtrIpRwZS'; 
-// config.import.rootfoldernamepath_DropBox = '/myhospitalstory.org/peabody/'
+//config.import.accessTokenDropBox = 
+// config.import.rootfoldernamepath_DropBox = 
 
 config.import.dropboxDownloadToLocal_folderName = 'download-history/dropbox'
 
